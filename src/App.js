8805084a22c9bd2637ca9df64531a103daf39562
Eric Newcomer <enewcomer23@gmail.com>
2020-10-19 10:36:16 -0700

    FIx page routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
-import Navigation from './components/Navigation';
-import Home from './components/Home';
-import Community from './components/Community';
-
+import "./App.css";
+import Navigation from "./components/Navigation";
+import Home from "./components/Home";
+import Community from "./components/Community";
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navigation />
-        <div className="Content">
-          <Route path="/" exact component={Home} />
-          <Route path="/community" exact component={Community} />
-        </div>
+        <br />
+        <Route path="/" exact component={Home} />
+        <Route path="/community" exact component={Community} />
       </div>
     </Router>
   );
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,45 +1,61 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom/Link";
-import {
-  Navbar,
-  // NavDropdown,
-  Nav,
-  Form,
-  FormControl,
-  Button,
-} from "react-bootstrap";
+import Link from "react-router-dom/Link";
+// import {
+//   Navbar,
+//   // NavDropdown,
+//   Nav,
+//   Form,
+//   FormControl,
+//   Button,
+// } from "react-bootstrap";
 
 export default class Navigation extends Component {
   render() {
     return (
-      <Navbar bg="light" expand="lg" fixed="top">
-        <Navbar.Brand href="#home">Jails2Jobs</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Form inline className="mr-6">
-            <FormControl
-              type="text"
-              placeholder="Search Jobs"
-              className="mr-sm-2"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Nav className="mr-auto">
-            <Nav.Link as={Link} to="/">
-              Find Jobs
-            </Nav.Link>
-            <Nav.Link as={Link} to="/community">
-              Community
-            </Nav.Link>
-          </Nav>
-          <Nav>
-            <Nav.Link >
-              Profile
-            </Nav.Link>
-            <Nav.Link href="">Settings</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand">Jails2Jobs</Link>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Find Jobs</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/community" className="nav-link">Community</Link>
+            </li>
+            {/* <li className="navbar-item">
+              <Link to="/user" className="nav-link">Create User</Link>
+            </li> */}
+          </ul>
+        </div>
+      </nav>
+      // <Navbar bg="light" expand="lg" fixed="top">
+      //   <Navbar.Brand href="#home">Jails2Jobs</Navbar.Brand>
+      //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      //   <Navbar.Collapse id="basic-navbar-nav">
+      //     <Form inline className="mr-6">
+      //       <FormControl
+      //         type="text"
+      //         placeholder="Search Jobs"
+      //         className="mr-sm-2"
+      //       />
+      //       <Button variant="outline-success">Search</Button>
+      //     </Form>
+      //     <Nav className="mr-auto">
+      //       <Nav.Link as={Link} to="/">
+      //         Find Jobs
+      //       </Nav.Link>
+      //       <Nav.Link as={Link} to="/community">
+      //         Community
+      //       </Nav.Link>
+      //     </Nav>
+      //     <Nav>
+      //       <Nav.Link >
+      //         Profile
+      //       </Nav.Link>
+      //       <Nav.Link href="">Settings</Nav.Link>
+      //     </Nav>
+      //   </Navbar.Collapse>
+      // </Navbar>
     );
   }
 }
