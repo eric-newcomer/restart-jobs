c5b728d7c0ba0efe019cf90f71f42d0eab949e1d
Eric Newcomer <enewcomer23@gmail.com>
2020-10-19 10:37:13 -0700

    Remove react-bootstrap navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -22,40 +22,9 @@ export default class Navigation extends Component {
             <li className="navbar-item">
               <Link to="/community" className="nav-link">Community</Link>
             </li>
-            {/* <li className="navbar-item">
-              <Link to="/user" className="nav-link">Create User</Link>
-            </li> */}
           </ul>
         </div>
       </nav>
-      // <Navbar bg="light" expand="lg" fixed="top">
-      //   <Navbar.Brand href="#home">Jails2Jobs</Navbar.Brand>
-      //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      //   <Navbar.Collapse id="basic-navbar-nav">
-      //     <Form inline className="mr-6">
-      //       <FormControl
-      //         type="text"
-      //         placeholder="Search Jobs"
-      //         className="mr-sm-2"
-      //       />
-      //       <Button variant="outline-success">Search</Button>
-      //     </Form>
-      //     <Nav className="mr-auto">
-      //       <Nav.Link as={Link} to="/">
-      //         Find Jobs
-      //       </Nav.Link>
-      //       <Nav.Link as={Link} to="/community">
-      //         Community
-      //       </Nav.Link>
-      //     </Nav>
-      //     <Nav>
-      //       <Nav.Link >
-      //         Profile
-      //       </Nav.Link>
-      //       <Nav.Link href="">Settings</Nav.Link>
-      //     </Nav>
-      //   </Navbar.Collapse>
-      // </Navbar>
     );
   }
 }
