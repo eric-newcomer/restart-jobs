6e889e5652d7049462c668a36f58ec4714e62fbe
Eric Newcomer <enewcomer23@gmail.com>
2021-01-18 20:42:54 -0800

    Modify Header to home, opps, calendar, and forum

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import HomeIcon from "@material-ui/icons/Home";
-import SupervisorAccountIcon from "@material-ui/icons/SupervisorAccount";
+import ForumIcon from '@material-ui/icons/Forum';
 import BusinessCenterIcon from "@material-ui/icons/BusinessCenter";
 import ChatIcon from "@material-ui/icons/Chat";
-import NotificationsIcon from "@material-ui/icons/Notifications";
+import EventIcon from '@material-ui/icons/Event';
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
 
@@ -26,23 +26,20 @@ function Header() {
   return (
     <div className="header">
       <div className="header__left">
-        {/* <h2>Restart Jobs</h2> */}
-        <img
+        <h2>Restart Jobs</h2>
+        {/* <img
           src="https://avatars0.githubusercontent.com/u/70076380?s=200&v=4"
           alt=""
-        />
-        <div class="header__search">
-          <SearchIcon />
-          <input type="text" placeholder="Search" />
-        </div>
+        /> */}
+        
+        <Link to="/" style={{ textDecoration: 'none' }}><HeaderOptions Icon={HomeIcon} title="Home" /></Link>
+        <Link to="/opportunities" style={{ textDecoration: 'none' }}><HeaderOptions Icon={BusinessCenterIcon} title="Opportunities" /></Link>
+        <Link to="/calendar" style={{ textDecoration: 'none' }}><HeaderOptions Icon={EventIcon} title="Calendar" /></Link>
+        <Link to="/forum" style={{ textDecoration: 'none' }}><HeaderOptions Icon={ForumIcon} title="Forum" /></Link>
       </div>
 
       <div className="header__right">
-        <Link to="/" style={{ textDecoration: 'none' }}><HeaderOptions Icon={HomeIcon} title="Home" /></Link>
-        <Link to="/network" style={{ textDecoration: 'none' }}><HeaderOptions Icon={SupervisorAccountIcon} title="My Network" /></Link>
-        <Link to="/jobs" style={{ textDecoration: 'none' }}><HeaderOptions Icon={BusinessCenterIcon} title="Jobs" /></Link>
-        <Link to="messages" style={{ textDecoration: 'none' }}><HeaderOptions Icon={ChatIcon} title="Messaging" /></Link>
-        <Link to="/notifications" style={{ textDecoration: 'none' }}><HeaderOptions Icon={NotificationsIcon} title="Notifications" /></Link>
+        
         <Link to="profile" style={{ textDecoration: 'none' }}><HeaderOptions
           avatar="https://avatars2.githubusercontent.com/u/20120289?s=460&u=bd9cc7c182ebfd45566b8f5e5b6025980a5fb8d1&v=4"
           title="Profile"
