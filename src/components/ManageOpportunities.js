bf29b4c0ca47624d54d064f69a731296a2421cd3
Eric Newcomer <enewcomer23@gmail.com>
2021-03-29 20:55:09 -0700

    chaneg to material ui table

--- src/components/ManageOpportunities.js
+++ src/components/ManageOpportunities.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import { makeStyles } from "@material-ui/core/styles";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
 
 import { db } from "../firebase/firebase";
 import { selectUser } from "../features/userSlice";
@@ -8,7 +16,14 @@ import Header from "../components/Header";
 import { PostRow } from "../components/PostRow";
 import "../styles/ManageOpportunities.css";
 
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+});
+
 function ManageOpportunities() {
+  const classes = useStyles();
   const [jobposts, setJobPosts] = useState([]);
 
   const user = useSelector(selectUser);
@@ -41,26 +56,31 @@ function ManageOpportunities() {
       <Header />
       <div className="manage">
         <h1>Manage Opportunities</h1>
-        <table>
-          <tr>
-            {/* <td>ID</td> */}
-            <td>Date Posted</td>
-            <td>Organization</td>
-            <td>Role</td>
-            <td>Location</td>
-            <td>Update</td>
-            <td>Delete</td>
-          </tr>
-          {jobposts.map((jp) => (
-            <PostRow
-              id={jp.id}
-              company={jp.data.company}
-              role={jp.data.role}
-              location={jp.data.location}
-              postedDate={formatDate(jp.data.postedDate)}
-            />
-          ))}
-        </table>
+        <TableContainer component={Paper}>
+          <Table className={classes.table} aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Date Posted</TableCell>
+                <TableCell align="right">Organization</TableCell>
+                <TableCell align="right">Role</TableCell>
+                <TableCell align="right">Location</TableCell>
+                <TableCell align="right">Options</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {jobposts.map((jp) => (
+                <PostRow
+                  id={jp.id}
+                  company={jp.data.company}
+                  role={jp.data.role}
+                  location={jp.data.location}
+                  postedDate={formatDate(jp.data.postedDate)}
+                />
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        
       </div>
     </>
   );
--- src/components/PostRow.js
+++ src/components/PostRow.js
@@ -1,21 +1,32 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
 
 import { db } from "../firebase/firebase";
 import { selectUser } from "../features/userSlice";
 import "../styles/Services.css";
 import Header from "../components/Header";
+import { Button } from "@material-ui/core";
 
 export const PostRow = ({ id, company, role, location, postedDate }) => {
   return (
     <>
-      <tr>
-         {/* <td>{id}</td> */}
-        <td>{postedDate}</td>
-        <td>{company}</td>
-        <td>{role}</td>
-        <td>{location}</td>
-      </tr>
+      <TableRow key={id}>
+        <TableCell component="th" scope="row">
+          {postedDate}
+        </TableCell>
+        <TableCell align="right">{company}</TableCell>
+        <TableCell align="right">{role}</TableCell>
+        <TableCell align="right">{location}</TableCell>
+        <TableCell align="right">
+          <Button>Edit</Button>
+          <Button>Delete</Button>
+        </TableCell>
+      </TableRow>
+      
     </>
   );
 };
